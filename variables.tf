c43043948da6710bbb2466b8d148001f881f2be2
fadipe ayobami <fadipe.ayobami@tarvostechnology.com>
2021-03-12 12:46:31 +0100

    commit msg

--- variables.tf
+++ variables.tf
@@ -1,10 +1,7 @@
 variable "project_id" {
   type        = string
   description = "The Google Cloud Project Id"
-  default =  "pubsub-306610"
 }
-
 variable "region" {
   type    = string
-  default = "europe-west2-a"
 }
